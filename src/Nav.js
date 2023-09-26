f472e25b1525eb6df576c9f41319ffb1f0934a41
김준식 <wnstlr8879@naver.com>
2023-09-27 00:32:18 +0900

    nav background 수정 및 scroll 이벤트 추가

--- src/Nav.js
+++ src/Nav.js
@@ -1,10 +1,26 @@
+import { useEffect, useState } from "react";
+
 function Nav() {
   const collapse = "collapse d-flex justify-content-end";
   const navbarCollapse = "collapse navbar-collapse";
+  const scroll = document.documentElement.scrollTop;
+  const clientWidth = document.documentElement.clientWidth;
+
+  const [navVisibility, setNavVisibility] = useState("invisible");
+
+  useEffect(() => {
+    scroll >= 200 && setNavVisibility("visible");
+    if (scroll <= 200) {
+      setNavVisibility("invisible");
+    }
+  }, [scroll]);
 
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary" id="navbar">
+      <nav
+        className={`navbar navbar-expand-lg bg-body-tertiary ${navVisibility}`}
+        id={`${scroll <= 200 ? "navbar" : "navbarBlur"}`}
+      >
         <div className="container-fluid">
           <a className="navbar-brand" href="#home">
             KJS's Portfolio
@@ -21,19 +37,10 @@ function Nav() {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div
-            className={`${
-              document.documentElement.clientWidth < 992
-                ? navbarCollapse
-                : collapse
-            }`}
+            className={`${clientWidth <= 992 ? navbarCollapse : collapse}`}
             id="navbarTogglerDemo02"
           >
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              {/* <li className="nav-item">
-                <a className="nav-link" aria-current="page" href="#home">
-                  {`< Home />`}
-                </a>
-              </li> */}
               <li className="nav-item">
                 <a className="nav-link" href="#about-section">
                   {`< About />`}
