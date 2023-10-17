294a17654aff4230345e3b76a75428fa10752e90
김준식 <wnstlr8879@naver.com>
2023-10-17 23:42:05 +0900

    배포 에러 재수정

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 
 // const topBtnEffectLine = createSlice({
 //   name: "topBtnEffectLine",
